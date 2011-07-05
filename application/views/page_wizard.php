368627c6b5bcf188c6a2b1de9c4786b18f411367
David Harker <github.lists@oneiria.co.uk>
2011-07-05 17:06:44 +0100

    added wizard template (needs making into a view still

---
+++ application/views/page_wizard.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <title>thermal-age.eu</title>
+        <link rel="stylesheet" href="reset-min.css" type="text/css" media="screen" charset="utf-8">
+        <link rel="stylesheet" href="thermal-age.css" type="text/css" media="screen" charset="utf-8">
+    </head>
+    <body>
+        <div id="mainWindow">
+            <div id="central">
+                
+                <div id="wizardControlBox" class="smartBox centreFloater">
+                    
+                    
+                    <div id="wizardScreen" class="smartBox wizScreenPos">
+                        <h1 class="first">Welcome to thermal-age.eu</h1>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nibh in lorem cursus tempus. Nunc vel venenatis dolor. Ut tempus nisl eu ipsum varius malesuada. Vivamus nec justo nec diam suscipit eleifend. Nulla tincidunt, nisl eu auctor condimentum, est risus dapibus magna, id convallis dui urna at elit. Donec id purus vitae nulla gravida malesuada in id ligula. Quisque pellentesque purus eget est adipiscing vehicula. Curabitur pretium, urna convallis lacinia mollis, tellus nulla vehicula eros, quis hendrerit magna eros ut erat. Maecenas adipiscing consectetur felis id iaculis. Aenean at mi leo, vestibulum vestibulum risus. Aliquam rutrum tortor a lacus molestie rutrum iaculis mi semper. Nam et aliquam ligula. Vivamus eget urna ac sem pretium suscipit. Nulla pharetra venenatis felis ac imperdiet. Vivamus aliquet augue id leo sagittis pellentesque. Nulla facilisi. Sed iaculis magna eget sapien mollis ornare.
+                        </p>
+                        <h1>In other news...</h1>
+                        <p>
+                            Curabitur eu risus tortor, pellentesque blandit eros. Duis sit amet arcu tortor, in sagittis arcu. Nulla mattis massa ac ipsum viverra aliquam. Nunc varius tristique bibendum. Ut gravida nisl ac nulla aliquet ullamcorper. Vestibulum faucibus nulla lacinia mi blandit vehicula id in ligula. Nulla pulvinar, quam eu vehicula pretium, magna leo venenatis felis, eu mollis leo odio vitae odio. Nulla vehicula, massa sed hendrerit porta, magna est tristique nunc, quis rhoncus sem tortor quis leo. Vivamus in egestas est.
+                        </p>
+
+
+                    </div>
+
+                    <div id="wizardRightColumn">
+                        <a href="/" id="thermalAgeLogo"></a>
+                        <a href="#somewhereelse6" id="synthesysLogo"></a>
+                        <ul class="menu">
+                            <li><a href="#here1" class="complete" title=""><div class="icon"></div>Model Reaction</a></li>
+                            <li><a href="#here2" class="error" title=""><div class="icon"></div>Site Location</a></li>
+                            <li><a href="#here3" class="complete" title=""><div class="icon"></div>Sample Age</a></li>
+                            <li><a href="#here4" class="current" title="">Excavation Date</a></li>
+                            <li><a href="#here5" title="">Burial</a></li>
+                            <li><a href="#here6" title="">Storage</a></li>
+                            <li><a href="#here7" title="">Sampling</a></li>
+
+
+                        </ul>
+                    </div>
+                    
+                    <div id="pageLinks">
+                        <ul>
+                            <li><a class="current" href="#somewhereelse1">home</a></li>
+                            <li><a href="#somewhereelse2">about</a></li>
+                            <li><a href="#somewhereelse3">legal</a></li>
+                            <li><a href="#somewhereelse4">citing</a></li>
+                            <li><a href="#somewhereelse5">help</a></li>
+                            <li><a href="#somewhereelse6">developers</a></li>
+                        </ul>
+                        
+                    </div>
+                    
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
