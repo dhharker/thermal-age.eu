1ef9314b4d4d6ab15fb3e04e56317f059ff8a66d
David Harker <github.lists@oneiria.co.uk>
2011-07-06 13:53:13 +0100

    final ci before cake migrate

--- application/views/page_wizard.php
+++ application/views/page_wizard.php
@@ -8,21 +8,17 @@
         <link rel="stylesheet" href="/assets/css/thermal-age-wizard.css" type="text/css" media="screen" charset="utf-8">
     </head>
     <body>
-        <div id="mainWindow">
+        <div id="gradientContainer"><div id="mainWindow">
             <div id="central">
                 
                 <div id="wizardControlBox" class="smartBox centreFloater">
                     
                     
                     <div id="wizardScreen" class="smartBox wizScreenPos">
-                        <h1 class="first">Welcome to thermal-age.eu</h1>
+                        <h1 class="first">This is a template</h1>
 
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nibh in lorem cursus tempus. Nunc vel venenatis dolor. Ut tempus nisl eu ipsum varius malesuada. Vivamus nec justo nec diam suscipit eleifend. Nulla tincidunt, nisl eu auctor condimentum, est risus dapibus magna, id convallis dui urna at elit. Donec id purus vitae nulla gravida malesuada in id ligula. Quisque pellentesque purus eget est adipiscing vehicula. Curabitur pretium, urna convallis lacinia mollis, tellus nulla vehicula eros, quis hendrerit magna eros ut erat. Maecenas adipiscing consectetur felis id iaculis. Aenean at mi leo, vestibulum vestibulum risus. Aliquam rutrum tortor a lacus molestie rutrum iaculis mi semper. Nam et aliquam ligula. Vivamus eget urna ac sem pretium suscipit. Nulla pharetra venenatis felis ac imperdiet. Vivamus aliquet augue id leo sagittis pellentesque. Nulla facilisi. Sed iaculis magna eget sapien mollis ornare.
-                        </p>
-                        <h1>In other news...</h1>
-                        <p>
-                            Curabitur eu risus tortor, pellentesque blandit eros. Duis sit amet arcu tortor, in sagittis arcu. Nulla mattis massa ac ipsum viverra aliquam. Nunc varius tristique bibendum. Ut gravida nisl ac nulla aliquet ullamcorper. Vestibulum faucibus nulla lacinia mi blandit vehicula id in ligula. Nulla pulvinar, quam eu vehicula pretium, magna leo venenatis felis, eu mollis leo odio vitae odio. Nulla vehicula, massa sed hendrerit porta, magna est tristique nunc, quis rhoncus sem tortor quis leo. Vivamus in egestas est.
+                            This space will contain all sorts of maps and whizzy, slidey things.
                         </p>
 
 
@@ -33,26 +29,25 @@
 <!--                        <a href="#somewhereelse6" id="synthesysLogo"></a>-->
                         <h3>Wizard Progress:</h3>
                         <ul class="menu">
-                            <li><a href="#here1" class="complete" title=""><div class="icon"></div>Sample Name <div class="blurbCon">&rArr; <strong>Pickled monkey brains #1 overlong...</strong></div></a></li>
+                            <li><a href="#here1" class="complete" title=""><div class="icon"></div>Specimen Name <div class="blurbCon">&rArr; <strong>Pickled monkey brains #1 overlong...</strong></div></a></li>
                             <li><a href="#here1" class="complete" title=""><div class="icon"></div>Model Reaction <div class="blurbCon">&rArr; DNA Depurination</div></a></li>
                             <li><a href="#here2" class="error" title=""><div class="icon"></div>Site Location <div class="blurbCon">&rArr; Error(s) - please amend!</div></a></li>
-                            <li><a href="#here3" class="complete" title=""><div class="icon"></div>Sample Age <div class="blurbCon">&rArr; 4500 BCE</div></a></li>
-                            <li><a href="#here4" class="current" title="">Excavation Date</a></li>
-                            <li><a href="#here5" class="future" title="">Burial</a></li>
+                            <li><a href="#here3" class="complete" title=""><div class="icon"></div>Specimen Age <div class="blurbCon">&rArr; Deposited: 4500 BCE</div></a></li>
+                            <li><a href="#here3" class="complete" title=""><div class="icon"></div>Excavation Date <div class="blurbCon">&rArr; Excavated: AD 2011</div></a></li>
+                            <li><a href="#here5" class="current" title="">Burial</a></li>
+                            <li><a href="#here5" class="future" title="">Storage</a></li>
                             
                         </ul>
 
                         <div style="height: 1em;"></div>
 
-                        <h3>Options:</h3>
-                        <ul class="options menu">
-                            <li><a href="#here1" class="save" title=""><div class="icon"></div>Save <div class="blurbCon">...your answers so far</div></a></li>
-                            <li><a href="#here2" class="load" title=""><div class="icon"></div>Load <div class="blurbCon">...previously saved answers</div></a></li>
-                            <li><a href="#here3" class="help" title=""><div class="icon"></div>Help <div class="blurbCon">...if you're stuck</div></a></li>
-                        </ul>
 
+                        <ul class="options menu" style="position: absolute; bottom: 1em;">
+                            
+                            <li><a href="#here1" title=""><div class="icon"></div>Help <div class="blurbCon">...for if you get stuck</div></a></li>
+                            <li><a href="#here1" title=""><div class="icon"></div>Login <div class="blurbCon">...to get MORE</div></a></li>
+                            <li><a href="#here1" title=""><div class="icon"></div>Feedback <div class="blurbCon">...helps make this site better!</div></a></li>
 
-                        <ul class="menu" style="position: absolute; bottom: 1em;">
                             <li><a href="http://www.synthesys.info/II_JRA_1.htm" title="Part of the SYNTHESYS project" id="synthesysLogoLink">&nbsp;</a></li>
                         </ul>
                     </div>
@@ -72,6 +67,6 @@
 
                 </div>
             </div>
-        </div>
+        </div></div>
     </body>
 </html>
