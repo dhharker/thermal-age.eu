548e2e721b8f42555e20230de9a8724c426dbc01
David Harker <github.lists@oneiria.co.uk>
2011-07-20 20:01:06 +0100

    stop minify inheriting default layout from AClr by setting empty

--- app/app_controller.php
+++ app/app_controller.php
@@ -39,6 +39,8 @@ class AppController extends Controller {
      * Global stuff goes here
      */
     function beforeFilter () {
+        parent::beforeFilter();
+
         $this->set('global_minified_javascript',$this->Minify->js(array(
               /*prod:'js/wizard_components.js',*/
             'js/jquery-1.5.1.min.js',
@@ -48,7 +50,9 @@ class AppController extends Controller {
             'js/jqf/jquery.form.js',
             'js/adapt/adapt.min.js',
         )));
-        parent::beforeFilter();
+
+        // The minify controller needs a blank layout otherwise it'll inherit from $layout above
+        $this->Minify->layout = '';
     }
 
 }
