49237d624a246765641b57ed0f52853c91062e6f
David Harker <github.lists@oneiria.co.uk>
2011-07-22 14:02:08 +0100

    tweaked error class and validators - looks like form data isn't getting sent to model properly

--- app/models/specimen.php
+++ app/models/specimen.php
@@ -4,14 +4,20 @@ class Specimen extends AppModel {
 	var $displayField = 'name';
 	var $validate = array(
 		'name' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'wtf',
-				'allowEmpty' => false,
-				//'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
+            'between' => array(
+                'rule' => array('between', 5, 15),
+                'required' => true,
+                'allowEmpty' => false,
+                'message' => 'Between 5 to 15 characters'
+            )
+		),
+        'code' => array(
+            'rule' => array('notEmpty'),
+            //'message' => 'wtf',
+            //'allowEmpty' => false,
+            'required' => true,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
 		),
 	);
 }
