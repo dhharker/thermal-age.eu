4d0625a9152fe7389c32331856cf887154e9d9bc
David Harker <github.lists@oneiria.co.uk>
2013-03-02 14:46:37 +0000

    added user very basic login and layed the groundwork for oauth login

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -2,13 +2,93 @@
 class UsersController extends AppController {
 
 	var $name = 'Users';
+        var $components = array ('Auth');
 
     function login() {
        //Auth Magic
     }
     function logout() {
-       //Leave empty for now.
+       $this->redirect($this->Auth->logout());
     }
+    
+    private function _loadOpauth () {
+        require_once (ROOT . DS . APP_DIR . DS . 'vendors' . DS . 'opauth'.DS.'lib'.DS.'Opauth'.DS.'opauth.conf.php');
+        require_once (ROOT . DS . APP_DIR . DS . 'vendors' . DS . 'opauth'.DS.'lib'.DS.'Opauth'.DS.'Opauth.php');
+
+        //define('CONF_FILE', dirname(__FILE__).'/'.'opauth.conf.php');
+        //define('OPAUTH_LIB_DIR', dirname(dirname(__FILE__)).'/lib/Opauth/');
+
+        $Opauth = new Opauth( $config );
+    }
+    
+    // Redirect user to google oauth login
+    function oauth () {
+        $this->_loadOpauth();
+    }
+    
+    // After user has been oauth'd by google, do stuff.
+    function oacb () {
+        $this->_loadOpauth();
+        
+        $response = null;
+
+switch($Opauth->env['callback_transport']) {
+	case 'session':
+		session_start();
+		$response = $_SESSION['opauth'];
+		unset($_SESSION['opauth']);
+		break;
+	case 'post':
+		$response = unserialize(base64_decode( $_POST['opauth'] ));
+		break;
+	case 'get':
+		$response = unserialize(base64_decode( $_GET['opauth'] ));
+		break;
+	default:
+		echo '<strong style="color: red;">Error: </strong>Unsupported callback_transport.'."<br>\n";
+		break;
+}
+
+/**
+ * Check if it's an error callback
+ */
+if (array_key_exists('error', $response)) {
+	echo '<strong style="color: red;">Authentication error: </strong> Opauth returns error auth response.'."<br>\n";
+}
+
+/**
+ * Auth response validation
+ * 
+ * To validate that the auth response received is unaltered, especially auth response that 
+ * is sent through GET or POST.
+ */
+else{
+	if (empty($response['auth']) || empty($response['timestamp']) || empty($response['signature']) || empty($response['auth']['provider']) || empty($response['auth']['uid'])) {
+		echo '<strong style="color: red;">Invalid auth response: </strong>Missing key auth response components.'."<br>\n";
+	} elseif (!$Opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason)) {
+		echo '<strong style="color: red;">Invalid auth response: </strong>'.$reason.".<br>\n";
+	} else {
+		echo '<strong style="color: green;">OK: </strong>Auth response is validated.'."<br>\n";
+
+		/**
+		 * It's all good. Go ahead with your application-specific authentication logic
+		 */
+	}
+}
+
+
+/**
+* Auth response dump
+*/
+echo "<pre>";
+print_r($response);
+echo "</pre>";
+        
+        // See if local user account exists
+        // Create it if not
+        // Log the user in
+    }
+    
 
 	function index() {
 		$this->User->recursive = 0;
--- app/views/users/login.ctp
+++ app/views/users/login.ctp
@@ -1,1 +1,22 @@
-login innit
+<div class="grid_7"><div class="smartbox">
+<h1>Login</h1>
+
+<?php echo $this->Form->create  ('User', array('url' => $this->here, 'class' => 'ui-corner-all', 'style' => "margin: 1em")); ?>
+
+<?= $this->Form->input('User.username', array(
+    'label' => 'Username/Email',
+    'placeholder' => 'e.g. me@example.com',
+));?>
+<?= $this->Form->input('User.password', array(
+    'label' => 'Password',
+    'placeholder' => '',
+));?>
+<?= $this->Form->submit("Login");?>
+
+<?php echo $this->Form->end(); ?>
+
+</div></div>
+<div class="grid_5"><div class="smartbox">
+<h2>External</h2>
+
+</div></div>
