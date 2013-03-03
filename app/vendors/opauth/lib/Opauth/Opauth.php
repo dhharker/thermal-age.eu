8d8eae9631536b1d4bbb85e5752195d9128cedbc
David Harker <github.lists@oneiria.co.uk>
2013-03-03 01:20:50 +0000

    infinite oauth redirect loop for the lose

--- app/vendors/opauth/lib/Opauth/Opauth.php
+++ app/vendors/opauth/lib/Opauth/Opauth.php
@@ -147,7 +147,7 @@ class Opauth {
                 // Hack to fix WHATTHEFUCK the fact that this can't handle a / in the query string for some reason.
                 // ...possibly a cake related reason but possibly not.
                 
-                if (preg_match ("/([^\/]+)\/?\?code=([A-Za-z0-9-_\/]+)/", $this->env['request'], $m)) {
+                if (preg_match ("/([^\/]+)\/?\?code=([^\/&]+)/", $this->env['request'], $m)) {
                     $this->env['params']['strategy'] = $m[1];
                     //$this->env['params']['action'] = $m[2];
                 }
