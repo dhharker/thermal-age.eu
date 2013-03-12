e11838400aa1e94db8d73512752883bd86111490
David Harker <github.lists@oneiria.co.uk>
2013-03-12 07:55:19 +0000

    fixed race condition on report load which was stopping the graphs from loading some of the time

--- app/webroot/js/job_report.js
+++ app/webroot/js/job_report.js
@@ -1,73 +1,72 @@
-$(function () {
+(function ($) {
+    $(document).ready(function () {
+        
+        console.log ("svg");
+        /* When the viewport is resized, scale embeds to match new parent width while maintaining
+        original aspect ratio. */
+        var embedIndex = 0;
+        $('.expand-embeds').find('embed').each (function () {
+                var did = 'embed-svg-' + embedIndex++;
+                var $this = $(this).hide();
+                var before = {
+                    width: $this.width(),
+                    height: $this.height()
+                };
+                var svgFile = $this.attr('src');
 
-    //console.log (svg,svgFile,did);
-    /* When the viewport is resized, scale embeds to match new parent width while maintaining
-    original aspect ratio. */
-    var embedIndex = 0;
-    $('.expand-embeds').find('embed').each (function () {
-            var did = 'embed-svg-' + embedIndex++;
-            var $this = $(this).hide();
-            var before = {
-                width: $this.width(),
-                height: $this.height()
-            };
-            var svgFile = $this.attr('src');
+                var div = $('<div></div>').attr('id',did).addClass('embed-svg').css(before);
 
-            var div = $('<div></div>').attr('id',did).addClass('embed-svg').css(before);
+                div.insertAfter($this).svg({
+                    onLoad: function() {
+                        //console.log (svg,svgFile,did);
+                        var embd = $('#'+did);
+                        var svg = embd.svg('get');
+                        svg.load(svgFile, {
+                            addTo: false,
+                            changeSize: false
+                        });
+                    },
+                    settings: {}
+                });
 
-            div.insertAfter($this).svg({
-                onLoad: function() {
-                    //console.log (svg,svgFile,did);
-                    var embd = $('#'+did);
-                    var svg = embd.svg('get');
-                    svg.load(svgFile, {
-                        addTo: false,
-                        changeSize: false
-                    });
-                },
-                settings: {}
+                $this.remove();
+
+        });
+
+        $('.expand-embeds').resize (function () {
+            var $pard = $(this);
+            $('.loading-spinner', $pard.parent()).hide({
+                'effect': 'blind',
+                'duration': 200
+            }, function () {
+                $(this).remove();
             });
-            
-            $this.remove();
+            $pard.find('.embed-svg svg').each (function () {
+                var before = {
+                    width: $(this).width(),
+                    height: $(this).height()
+                };
 
+                var nw = $pard.width();
+                var after = {
+                    width: nw,
+                    height: ((nw / $(this).width()) * $(this).height())
+                };
+
+
+                // For some reason this isn't either working or causing an error
+
+                $(this).animate (after, {
+                    duration: 200,
+                    complete: function () {
+                        $(this).css (after);
+
+                    },
+                    easing: 'linear'
+                }).parent().css({height: 'auto', width: 'auto'});
+            });
+        }).resize();
     });
-
-    $('.expand-embeds').resize (function () {
-        var $pard = $(this);
-        $('.loading-spinner', $pard.parent()).hide({
-            'effect': 'blind',
-            'duration': 200
-        }, function () {
-            $(this).remove();
-        });
-        $pard.find('.embed-svg svg').each (function () {
-            var before = {
-                width: $(this).width(),
-                height: $(this).height()
-            };
-
-            var nw = $pard.width();
-            var after = {
-                width: nw,
-                height: ((nw / $(this).width()) * $(this).height())
-            };
-
-            
-            // For some reason this isn't either working or causing an error
-            
-            $(this).animate (after, {
-                duration: 200,
-                complete: function () {
-                    $(this).css (after);
-                    
-                },
-                easing: 'linear'
-            }).parent().css({height: 'auto', width: 'auto'});
-            //*/
-            //console.log (this, before, after);
-            //$(this).css(after)
-        });
-    });
-});
+}(jQuery));
 
 
